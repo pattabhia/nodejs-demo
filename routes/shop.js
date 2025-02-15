4b0f806eb04641b2e94cf1a2cb9ec6f9d64f7354
pattabhia <pattabhi.amperayani@gmail.com>
2025-02-15 12:35:26 +0530

    adding css and utils

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
-  });
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
 
 module.exports = router;
